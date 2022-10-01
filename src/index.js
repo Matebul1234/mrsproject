260a1c196ba3f4a61dede0ff0fd6686ce2a1a593
Matebul <mohammadmatebul047890@gmail.com>
2022-10-01 17:26:41 +0530

    react project first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Components/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import MaincategoryContextProvider from './Store/MaincategoryContextProvider'
+import SubcategoryContextProvider from './Store/SubcategoryContextProvider'
+import BrandContextProvider from './Store/BrandContextProvider'
+import ProductContextProvider from './Store/ProductContextProvider'
+import UserContextProvider from './Store/UserContextProvider'
+import ContactContextProvider from './Store/ContactContextProvider'
+import NewslatterContextProvider from './Store/NewslatterContextProvider'
+import CartContextProvider from './Store/CartContextProvider'
+import WishlistContextProvider from './Store/WishlistContextProvider'
+import CheckoutContextProvider from './Store/CheckoutContextProvider'
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+        <MaincategoryContextProvider>
+            <SubcategoryContextProvider>
+                <BrandContextProvider>
+                    <ProductContextProvider>
+                        <UserContextProvider>
+                            <ContactContextProvider>
+                               <NewslatterContextProvider>
+                                   <CartContextProvider>
+                                    <WishlistContextProvider>
+                                        <CheckoutContextProvider>
+                                            <App />
+                                        </CheckoutContextProvider>
+                                    </WishlistContextProvider>
+                                   </CartContextProvider>
+                               </NewslatterContextProvider>
+                            </ContactContextProvider>
+                        </UserContextProvider>
+                    </ProductContextProvider>
+                </BrandContextProvider>
+            </SubcategoryContextProvider>
+        </MaincategoryContextProvider>
+    </>
+)
